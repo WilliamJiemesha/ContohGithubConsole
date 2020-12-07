6d7eed9d89ed7c6f1d11059cb65d6bef02596fa7
WilliamJiemesha <67468607+WilliamJiemesha@users.noreply.github.com>
2020-12-07 09:12:37 +0800

    Testing

---
+++ GithubContoh/GithubContoh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithubContoh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
